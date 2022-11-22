73f6cba52dace38fef1c197aa0a19b80c73e1d90
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-22 15:42:08 -0300

    feat(challenges): Create study shcedulles

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period, target_time) -> int:
+    try:
+        quantity_students: int = 0
+        for init_time, end_time in permanence_period:
+            if init_time <= target_time <= end_time:
+                quantity_students += 1
+        return quantity_students
+    except TypeError:
+        return None
