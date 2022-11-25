16493f66215d65dae6436b395d261d6d759ea36f
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-11-25 20:30:00 -0300

    feat: Anagrams falta arrumar complexidade

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,17 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_anagram(first_string: str, second_string: str) -> tuple:
+    sortedFirst = sortString(first_string.lower())
+    sortedSecond = sortString(second_string.lower())
+    print(sortedFirst, sortedSecond, sortedSecond == sortedFirst)
+    return (sortedFirst, sortedSecond, sortedSecond == sortedFirst)
+
+
+def sortString(valueString: str) -> str:
+    sort = list(valueString)
+    sortUpdated = list(valueString)
+    for index in valueString:
+        for i in range(len(valueString) - 1, 0, -1):
+            if sortUpdated[i] < sortUpdated[i - 1]:
+                sort[i] = sortUpdated[i - 1]
+                sort[i - 1] = sortUpdated[i]
+                sortUpdated = sort.copy()
+    return "".join(sort)
